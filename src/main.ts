8f2feeea783009bacaf850929c18ecb0669e3ec7
CuongWSL <huucuongyd@gmail.com>
2026-08-16 14:43:19 +0700

    setup allow header

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:5173'],
     credentials: true,
+    allowedHeaders: ['Authorization', 'Content-Type', 'Accept'],
   });
   setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
